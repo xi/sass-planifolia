fb3ba534a64ee33300c8b2e04de54b36b8133313
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-03-26 21:59:04 +0100

    add alpha-constructor and getter functions

--- test/lab.js
+++ test/lab.js
@@ -24,4 +24,55 @@ describe('lab', function() {
       sassaby.func('lch').calledWithArgs('32.30259', '133.80605', '-0.93744').equals('blue');
     });
   });
+
+  describe('lab-lightness', function() {
+    it('white', function() {
+      shared.similar(sassaby.func('lab-lightness').calledWithArgs('white'), 100);
+    });
+    it('black', function() {
+      shared.similar(sassaby.func('lab-lightness').calledWithArgs('black'), 0);
+    });
+    it('red', function() {
+      shared.similar(sassaby.func('lab-lightness').calledWithArgs('red'), 53.23288);
+    });
+    it('blue', function() {
+      shared.similar(sassaby.func('lab-lightness').calledWithArgs('blue'), 32.30259);
+    });
+  });
+
+  describe('lab-chroma', function() {
+    it('white', function() {
+      shared.similar(sassaby.func('lab-chroma').calledWithArgs('white'), 0, 0.0001);
+    });
+    it('black', function() {
+      shared.similar(sassaby.func('lab-chroma').calledWithArgs('black'), 0);
+    });
+    it('red', function() {
+      shared.similar(sassaby.func('lab-chroma').calledWithArgs('red'), 104.57421);
+    });
+    it('blue', function() {
+      shared.similar(sassaby.func('lab-chroma').calledWithArgs('blue'), 133.80605);
+    });
+  });
+
+  describe('lab-hue', function() {
+    it('white', function() {
+      shared.similar(sassaby.func('lab-hue').calledWithArgs('white'), 0);
+    });
+    it('black', function() {
+      shared.similar(sassaby.func('lab-hue').calledWithArgs('black'), 0);
+    });
+    it('red', function() {
+      shared.similar(sassaby.func('lab-hue').calledWithArgs('red'), 0, 1);
+    });
+    it('yellow', function() {
+      shared.similar(sassaby.func('lab-hue').calledWithArgs('yellow'), Math.PI * 0.5, 1);
+    });
+    it('green', function() {
+      shared.similar(sassaby.func('lab-hue').calledWithArgs('green'), Math.PI, 1);
+    });
+    it('blue', function() {
+      shared.similar(sassaby.func('lab-hue').calledWithArgs('blue'), Math.PI * -0.5, 1);
+    });
+  });
 });
